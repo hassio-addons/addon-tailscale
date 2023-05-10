8a11c9104a85ef15da687b43bda7890c450972fa
Laszlo Magyar <lmagyar1973@gmail.com>
2023-05-10 20:40:24 +0200

    add local subnets to ip rules

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -5,6 +5,14 @@
 # S6 Overlay stage2 hook to customize services
 # ==============================================================================
 
+# Disable protect-subnets service when userspace-networking is enabled
+if ! bashio::config.has_value "userspace_networking" || \
+    bashio::config.true "userspace_networking";
+then
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/protect-subnets
+    rm /etc/s6-overlay/s6-rc.d/post-tailscaled/dependencies.d/protect-subnets
+fi
+
 # Disable taildrop service when it is has been explicitly disabled
 if bashio::config.false 'taildrop'; then
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/taildrop
