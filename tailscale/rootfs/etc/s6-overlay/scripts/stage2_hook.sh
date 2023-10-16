f20f925a1efa84d84e1263edbb81f5193273b63f
Laszlo Magyar <lmagyar1973@gmail.com>
2023-10-16 11:53:57 +0200

    code enhancement

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -5,18 +5,23 @@
 # S6 Overlay stage2 hook to customize services
 # ==============================================================================
 
-# Disable protect-subnets service when userspace-networking is enabled
+# Disable protect-subnets service when userspace-networking is enabled or accepting routes is disabled
+if ! bashio::config.has_value "userspace_networking" || \
+    bashio::config.true "userspace_networking" || \
+    bashio::config.false "accept_routes";
+then
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/protect-subnets
+    rm /etc/s6-overlay/s6-rc.d/post-tailscaled/dependencies.d/protect-subnets
+fi
+
 # Disable mss-clamping service when userspace-networking is enabled
 if ! bashio::config.has_value "userspace_networking" || \
     bashio::config.true "userspace_networking";
 then
-    rm /etc/s6-overlay/s6-rc.d/user/contents.d/protect-subnets
-    rm /etc/s6-overlay/s6-rc.d/post-tailscaled/dependencies.d/protect-subnets
-
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/mss-clamping
 fi
 
-# Disable taildrop service when it is has been explicitly disabled
+# Disable taildrop service when it has been explicitly disabled
 if bashio::config.false 'taildrop'; then
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/taildrop
 fi
