13ae8d0bcf0c61498168fa397609aebb860bbd89
Laszlo Magyar <lmagyar1973@gmail.com>
2023-12-16 18:28:24 +0100

    merge proxy and funnel into serve
    # Conflicts:
    #	tailscale/rootfs/etc/s6-overlay/s6-rc.d/funnel/run
    #	tailscale/rootfs/etc/s6-overlay/s6-rc.d/serve/run

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -26,14 +26,7 @@ if bashio::config.false 'taildrop'; then
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/taildrop
 fi
 
-# Disable proxy service when it has not been explicitly enabled
+# Disable serve service when proxy and/or funnel has not been explicitly enabled
 if ! bashio::config.true 'proxy'; then
-    rm /etc/s6-overlay/s6-rc.d/user/contents.d/proxy
-    rm /etc/s6-overlay/s6-rc.d/certificate/dependencies.d/proxy
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/serve
 fi
-
-# Disable funnel service when it has not been explicitly enabled
-if ! bashio::config.true 'proxy' || ! bashio::config.true 'funnel'; then
-    rm /etc/s6-overlay/s6-rc.d/user/contents.d/funnel
-fi
-
