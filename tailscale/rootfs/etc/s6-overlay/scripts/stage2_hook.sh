7d2ac6eb63da0089a9e83ac26395258b79db967b
Laszlo Magyar <lmagyar1973@gmail.com>
2025-01-15 13:08:13 +0100

    fix dnsmasq port and when to run

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -58,6 +58,15 @@ if bashio::var.has_value "${proxy_and_funnel_port}"; then
     bashio::addon.option 'proxy_and_funnel_port'
 fi
 
+# Disable dnsmasq service when userspace-networking is enabled or accepting dns is disabled
+if ! bashio::config.has_value "userspace_networking" || \
+    bashio::config.true "userspace_networking" || \
+    bashio::config.false "accept_dns";
+then
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/dnsmasq
+    rm /etc/s6-overlay/s6-rc.d/tailscaled/dependencies.d/dnsmasq
+fi
+
 # Disable protect-subnets service when userspace-networking is enabled or accepting routes is disabled
 if ! bashio::config.has_value "userspace_networking" || \
     bashio::config.true "userspace_networking" || \
