ba266a755342159b87e80d7f129b87692f1f854e
Laszlo Magyar <lmagyar1973@gmail.com>
2025-02-21 22:21:04 +0100

    forwarding - remove option and docs

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -14,10 +14,9 @@ then
     rm /etc/s6-overlay/s6-rc.d/post-tailscaled/dependencies.d/protect-subnets
 fi
 
-# Disable forwarding service when userspace-networking is enabled or forwarding to host is disabled
+# Disable forwarding service when userspace-networking is enabled
 if ! bashio::config.has_value "userspace_networking" || \
-    bashio::config.true "userspace_networking" || \
-    bashio::config.false "forward_to_host";
+    bashio::config.true "userspace_networking";
 then
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/forwarding
 fi
