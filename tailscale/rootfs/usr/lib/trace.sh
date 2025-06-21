d493d0022944db27fecdb05c1599d008bcc35f93
Laszlo Magyar <lmagyar1973@gmail.com>
2025-06-21 23:01:28 +0200

    minor refactor - dns

--- tailscale/rootfs/usr/lib/trace.sh
+++ tailscale/rootfs/usr/lib/trace.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+
 # ------------------------------------------------------------------------------
 # Checks if we are currently running in trace mode, based on the bashio log module.
 # ------------------------------------------------------------------------------
-function is_bashio_log_level_trace() {
+function bashio_custom::trace() {
     if [[ "${__BASHIO_LOG_LEVEL}" -lt "${__BASHIO_LOG_LEVEL_TRACE}" ]]; then
         return "${__BASHIO_EXIT_NOK}"
     fi
