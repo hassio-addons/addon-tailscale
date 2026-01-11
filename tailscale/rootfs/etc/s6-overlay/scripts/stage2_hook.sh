a9f47721f77d82351759ef01ce7890b9692dee1d
Laszlo Magyar <lmagyar1973@gmail.com>
2026-01-11 21:43:57 +0100

    improve error handling, thanks bunny

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -62,9 +62,14 @@ fi
 # Rename changed options
 tags=$(bashio::jq "${options}" '.tags | select(.!=null)')
 if bashio::var.has_value "${tags}"; then
-    bashio::log.info 'Renaming tags option to advertise_tags'
+    try bashio::addon.option 'advertise_tags' "^${tags}"
+    if ((TRY_ERROR)); then
+        bashio::log.warning "The tags option value is invalid, tags option is dropped, using default no advertise_tags."
+        bashio::log.warning "The invalid tags option value is: '${tags}'"
+    else
+        bashio::log.info "Successfully renamed tags option to advertise_tags"
+    fi
     bashio::addon.option 'tags'
-    bashio::addon.option 'advertise_tags' "^${tags}"
 fi
 
 # Disable protect-subnets service when userspace-networking is enabled or accepting routes is disabled
